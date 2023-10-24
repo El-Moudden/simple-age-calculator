75043dc72fd94d8f14954dd57c019e1db396e755
El Moudden Abdelhalim <117116526+El-Moudden@users.noreply.github.com>
2023-10-24 22:05:32 +0100

    Add files via upload
    #js #age-calculator #html #css

---
+++ main.js
@@ -0,0 +1,36 @@
+let btn = document.getElementById("btnInput");
+
+let dateInput = document.getElementById("dateInput") ;
+
+function calculateAge(){
+  let birthday = dateInput.value
+  // console.log(birthday)
+
+  if (birthday== ""){
+    document.getElementById("result").textContent = "Please Enter Your Birthday"
+  } else {
+    let age = getAge(birthday)
+    document.getElementById("result").textContent = `Your Age is ${age}` ;
+  }
+}
+
+function getAge(birthday) {
+  let currentDate = new Date() ;
+  let birthdayValue = new Date(birthday)
+  age = currentDate.getFullYear()-birthdayValue.getFullYear()
+  let month = currentDate.getMonth()-birthdayValue.getMonth();
+  
+   
+  if (currentDate.getMonth() < birthdayValue.getMonth() || currentDate.getMonth() == birthdayValue.getMonth() && currentDate.getDate() < birthdayValue.getDate()) {
+
+    age--
+    
+  }
+  return age ;
+  
+}
+
+
+
+
+btn.addEventListener("click", calculateAge)
